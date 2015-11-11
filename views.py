479c6bf516ad5bdc975ffe122a47f5e71123f4f9
Alex <alex@itsec.im>
2015-11-11 22:45:35 +0000

    Minor changes

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.utils import timezone
